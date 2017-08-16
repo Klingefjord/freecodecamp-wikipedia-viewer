130639db3bfb90b200ddfd6c2a4095e244bdb58f
Oliver Klingefjord <oliver.klingefjord@hyperisland.se>
2017-08-16 20:10:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
-var search = "#searchfield";
-var results = "#results";
-var wrap = "#wrapper";
-var url;
+//var search = "#searchfield";
+//var results = "#results";
+//var wrap = "#wrapper";
+//var url;
 
 //event handler for searchfield
 $(search).keypress(function(key){
   //if enter is pressed
   if (event.which === 13) {
-    submitSearch($(search).val());
+    submitSearch($(#searchfield).val());
   }
 });
 
 function submitSearch(input) {
   $(results).empty(); //empty div
-  $.getJSON(setUrl(input), function(data){
+  $.getJSON(getUrlWithInput(input), function(data){
     updateDocument(data);
   });
 }
 
-function setUrl(input) {
-  url = "https://en.wikipedia.org//w/api.php?action=query&prop=extracts|info&format=json&exsentences=1&excontinue=1&exlimit=10&exintro=&explaintext=&inprop=url&generator=search&redirects=&gsrsearch=" + input + "&gsrlimit=10&callback=?";
-  return url;
+function getUrlWithInput(input) {
+  return "https://en.wikipedia.org//w/api.php?action=query&prop=extracts|info&format=json&exsentences=1&excontinue=1&exlimit=10&exintro=&explaintext=&inprop=url&generator=search&redirects=&gsrsearch=" 
+    + input 
+    + "&gsrlimit=10&callback=?";
 }
 
 function updateDocument(data) {
   for (i in data.query.pages) {
       var link = data.query.pages[i].fullurl;
       var title = data.query.pages[i].title;
-      var extract = "";
-      if (data.query.pages[i].extract) {
-        extract = data.query.pages[i].extract;
-      }
-      $(results).append("<a target ='_blank' href='" + link + "'><div class='result'><h1>" + title + "</h1><p>" + extract + "</p></div></a>");
+    
+      
+      var extract = data.query.pages[i].extract ? data.query.pages[i].extract || "";
+   
+    //  if (data.query.pages[i].extract) {
+    //    extract = data.query.pages[i].extract;
+    //  }
+    
+      $(#results).append(
+        "<a target ='_blank' href='" + link + "'><div class='result'><h1>" 
+          + title 
+          + "</h1><p>" 
+          + extract 
+          + "</p></div></a>");
     }
   }
 
